0c94a7f8b27e98fed5be6719854172da4f37022e
Instructor Alex White <alex.white@codefellows.com>
2022-07-12 14:23:51 -0700

    class 39 demo for Location

--- class-31/demo/app/src/main/java/com/zork/class27demo/activities/AddProductActivity.java
+++ class-31/demo/app/src/main/java/com/zork/class27demo/activities/AddProductActivity.java
@@ -1,6 +1,7 @@
 package com.zork.class27demo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.api.graphql.model.ModelQuery;
@@ -8,8 +9,15 @@ import com.amplifyframework.core.Amplify;
 import com.amplifyframework.core.model.temporal.Temporal;
 import com.amplifyframework.datastore.generated.model.Contact;
 import com.amplifyframework.datastore.generated.model.Product;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.zork.class27demo.R;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,7 +40,8 @@ public class AddProductActivity extends AppCompatActivity {
     String[] categories = {"Clothes", "Electronics", "Perishable Goods", "Office Supplies", "Misc"};
     Spinner contactSpinner = null;
     CompletableFuture<List<Contact>> contactsFuture = null;
-
+    FusedLocationProviderClient fusedLocationClient = null;
+    Geocoder geocoder;
 
 
     @Override
@@ -40,14 +49,21 @@ public class AddProductActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
         contactsFuture = new CompletableFuture<>();
+
+        //Step 2: request permissions
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);  // hardcoded to 1; you can pick anything between 1 and 65535
+        // Step 3: give fusedLocationClient value
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        fusedLocationClient.flushLocations();
+
+
         setUpSpinner();
         setUpSaveButton();
-
 
     }
 
 
-    private void setUpSpinner(){
+    private void setUpSpinner() {
         Spinner productCategorySpinner = findViewById(R.id.addAProductCategoryInput);
         contactSpinner = findViewById(R.id.addAProductContactSpinner);
         Amplify.API.query(
@@ -82,7 +98,7 @@ public class AddProductActivity extends AppCompatActivity {
                 categories));
     }
 
-    private void setUpSaveButton(){
+    private void setUpSaveButton() {
         Spinner productCategorySpinner = findViewById(R.id.addAProductCategoryInput);
         Button saveNewProductButton = findViewById(R.id.addAProductSaveButton);
         saveNewProductButton.setOnClickListener(new View.OnClickListener() {
@@ -105,24 +121,48 @@ public class AddProductActivity extends AppCompatActivity {
                 }
                 Contact selectedContact = contacts.stream()
                         .filter(c -> c.getFullName().equals(selectedContactString)).findAny().orElseThrow(RuntimeException::new);
+//                saveProduct(productName, productDescription, currentDateString, productCategorySpinner, selectedContact);
 
-                // TODO: LOOK AT THE MODEL BUILDER
-                Product newProduct = Product.builder()
-                        .name(productName)
-                        .description(productDescription)
-                        .dateCreated(new Temporal.DateTime(currentDateString))
-                        .productCategory(productCategorySpinner.getSelectedItem().toString())
-                        .contactPerson(selectedContact)
-                        .build();
-                // TODO: THIS IS HOW WE CUD DynamoDB
-                Amplify.API.mutate(
-                        ModelMutation.create(newProduct), // making a Graphql request to the cloud
-                        // ModelMutation.update()
-                        //ModelMutation.delete()
-                        successResponse -> Log.i(TAG, "AddProductActivity.onCreate(): made a product successfully"),  // success callback
-                        failureResponse -> Log.i(TAG, "AddProductActivity.onCreate(): failed with this response: " + failureResponse)  // failure callback
-                );
+                if (ActivityCompat.checkSelfPermission(AddProductActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(AddProductActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    Log.e(TAG, "Application does not have access to either ACCESS_FINE_LOCATION or ACCESS_COARSE_LOCATION!");
+                    return;
+                }
+                fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+                    if (location == null) {
+                        Log.e(TAG, "Location was null");
+                    }
+                    String currentLatitude = Double.toString(location.getLatitude());
+                    String currentLongitude = Double.toString(location.getLongitude());
+                    Log.i(TAG, "Our Latitude: " + currentLatitude);
+                    Log.i(TAG, "Our Longitude: " + currentLongitude);
+                });
             }
         });
+    }
+
+    public void saveProduct(String productName, String productDescription, String currentDateString, Spinner productCategorySpinner, Contact selectedContact){
+        // TODO: LOOK AT THE MODEL BUILDER
+        Product newProduct = Product.builder()
+                .name(productName)
+                .description(productDescription)
+                .dateCreated(new Temporal.DateTime(currentDateString))
+                .productCategory(productCategorySpinner.getSelectedItem().toString())
+                .contactPerson(selectedContact)
+                .build();
+        // TODO: THIS IS HOW WE CUD DynamoDB
+        Amplify.API.mutate(
+                ModelMutation.create(newProduct), // making a Graphql request to the cloud
+                // ModelMutation.update()
+                //ModelMutation.delete()
+                successResponse -> Log.i(TAG, "AddProductActivity.onCreate(): made a product successfully"),  // success callback
+                failureResponse -> Log.i(TAG, "AddProductActivity.onCreate(): failed with this response: " + failureResponse)  // failure callback
+        );
     }
 }
